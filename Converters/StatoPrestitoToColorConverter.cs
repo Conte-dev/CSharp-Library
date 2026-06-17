b5fabb97088b89775a11ce7fdbc1c021f6fa738b
Conte-Developer <samueledone08@gmail.com>
2026-06-17 16:34:05 +0200

    Aggiunta dei converters

---
+++ Converters/StatoPrestitoToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using BibliotecaWPF.Models;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BibliotecaWPF.Converters
+{
+    public class StatoPrestitoToColorConverter : IValueConverter
+    {
+        private static readonly Brush AttivoBrush = Brushes.White;
+        private static readonly Brush InRitardoBrush =
+            (Brush)new BrushConverter().ConvertFrom("#FFDDDD");
+        private static readonly Brush RestituitoBrush =
+            (Brush)new BrushConverter().ConvertFrom("#DDFFDD");
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not StatoPrestito stato)
+                return AttivoBrush;
+
+            return stato switch
+            {
+                StatoPrestito.InRitardo => InRitardoBrush,
+                StatoPrestito.Restituito => RestituitoBrush,
+                _ => AttivoBrush
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ Converters/StatoPrestitoToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using BibliotecaWPF.Models;
+
+namespace BibliotecaWPF.Converters
+{
+    public class StatoPrestitoToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not StatoPrestito stato)
+                return string.Empty;
+
+            return stato switch
+            {
+                StatoPrestito.Attivo => "⏳",
+                StatoPrestito.InRitardo => "⚠️",
+                StatoPrestito.Restituito => "✔️",
+                _ => string.Empty
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
